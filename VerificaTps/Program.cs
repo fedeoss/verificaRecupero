5bb2d82754276977209ed3c713f3781543c9ba6c
fedeoss <federica.zani2003@gmail.com>
2021-03-31 10:20:07 +0200

    realizzazione primo thread(e inizializzazione secondo)

---
+++ VerificaTps/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace VerificaTps
+{
+    //Realizzare un programma in Console che contenga tre thread concorrenti. Il primo thread deve
+//definire 3 variabili Sub1(valore assegnato a vosto piacere), Sub2(valore assegnato a vostro
+//piacere), Sottrazione e dopo aver realizzato la sottrazione tra Sub1 e Sub2 e aver caricato nella
+//variabile Sottrazione il risultato preveda la stampa di tale valore per 5 volte a video.
+//Il secondo thread deve contenere un ciclo che si ripete 10 volte e che prevede di stampare a video la
+//tabellina del numero 37 (ovvero il risultato di 37*1, 37*2, 37*3 ecc).
+Il terzo thread deve prevedere la stampa a video del tuo cognome per 20 volte.
+I tre thread dovranno essere avviati contemporaneamente e si dovrà prevedere che il terzo thread
+termini sempre prima degli altri due.
+Il progetto dovrà essere caricato su una repository GitHub e dovrà contenere 3 COMMIT:
+il primo alla creazione del primo thread, il secondo alla creazione del secondo thread, il terzo al
+termine del progetto.
+Potete utilizzare sia la bash GIT che GitExtension (o software GUI simili)
+Consegnare il link al progetto GitHub.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(new ThreadStart(Sottrazione));
+            t1.Start();
+            Thread t2 = new Thread(new ThreadStart(Tabellina));
+            t2.Start();
+        }
+        public static void Sottrazione()
+        {
+            int Sub1 = 7;
+            int Sub2 = 5;
+            int sottrazione=Sub1-Sub2;
+            Console.WriteLine("sottrazione =" + sottrazione);
+            Console.WriteLine("sottrazione =" + sottrazione); 
+            Console.WriteLine("sottrazione =" + sottrazione);
+            Console.WriteLine("sottrazione =" + sottrazione);
+            Console.WriteLine("sottrazione =" + sottrazione);
+
+        }
+        public static void Tabellina()
+        {
+
+        }
+    }
+}
